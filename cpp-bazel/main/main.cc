9b683d5331a9b045a02a860c2629b5b91d1ed4bd
David Chen <davidchen94@outlook.com>
2022-08-19 05:46:41 -0700

    -

--- cpp-bazel/main/main.cc
+++ cpp-bazel/main/main.cc
@@ -1,10 +1,17 @@
 #include <crow.h>
+#include <fmt/ranges.h>
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "lib/my_str.h"
 
 int main() {
+  std::vector<std::string> v = {"boodbye", "world"};
+
+  fmt::print("{}\n", v);
+
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/")([]() { return MyStr("goodbye world\n").c_str(); });
