101b7c86ed984e743297a7f3b02b11ee2c37ad01
David Chen <david0x0922@gmail.com>
2023-11-09 04:08:31 +0000

    -

--- img-http/main.py
+++ img-http/main.py
@@ -1,8 +1,9 @@
+import os
+import sys
+
+from flask import Flask, make_response, request
 from flask_cors import CORS
 from requests.compat import urljoin
-import sys
-import os
-from flask import Flask, make_response, request
 
 img_dir = None
 img_urls = None
@@ -15,13 +16,18 @@ def make_img_url(filename):
   return urljoin(request.host_url, app.static_url_path + '/' + filename)
 
 
+def is_img(path):
+  return path.split('.')[-1].lower() in ['gif', 'jpeg', 'jpg', 'png', 'webp']
+
+
 def load_img_urls():
   global img_urls
   img_urls = []
 
   for root, _, files in os.walk(img_dir):
     for filename in files:
-      img_urls.append(make_img_url(os.path.relpath(root + '/' + filename, img_dir)))
+      if is_img(filename):
+        img_urls.append(make_img_url(os.path.relpath(root + '/' + filename, img_dir)))
 
   img_urls.sort()
 
@@ -31,7 +37,8 @@ def imgs():
   if not img_urls:
     load_img_urls()
 
-  html = '\n'.join(f'<img src="{url}" style="max-width: 100%;" />' for url in img_urls)
+  style = 'display: block; margin: 0 auto; max-width: 100%;'
+  html = '\n'.join(f'<img src="{url}" style="{style}" />' for url in img_urls)
 
   return make_response(html, 200)
 
