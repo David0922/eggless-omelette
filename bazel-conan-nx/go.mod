186a2622a8bcd8b7f195c96f728dd41f3e1336a7
David Chen <david0x0922@gmail.com>
2025-02-14 06:05:42 -0800

    remove chi

--- bazel-conan-nx/go.mod
+++ bazel-conan-nx/go.mod
@@ -4,14 +4,16 @@ go 1.22.3
 
 require (
 	connectrpc.com/connect v1.18.1
-	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/cors v1.2.1
+	github.com/rs/cors v1.11.1
+	github.com/rs/zerolog v1.33.0
 	golang.org/x/net v0.35.0
 	google.golang.org/grpc v1.70.0
 	google.golang.org/protobuf v1.36.5
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
--- bazel-conan-nx/go_connectrpc_server/main.go
+++ bazel-conan-nx/go_connectrpc_server/main.go
@@ -4,39 +4,45 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	"connectrpc.com/connect"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
+	"github.com/rs/cors"
+	"github.com/rs/zerolog"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
 	pb "dummy.mofu.dev/protos/gen_go"
-	pbConn "dummy.mofu.dev/protos/gen_go/gen_go_connect"
+	connectPb "dummy.mofu.dev/protos/gen_go/gen_go_connect"
 )
 
-type server struct{}
+type server struct {
+	logger zerolog.Logger
+}
 
 func (s *server) Add(
 	ctx context.Context,
 	req *connect.Request[pb.AddRequest],
 ) (*connect.Response[pb.AddResponse], error) {
-	log.Printf("request: %+v\n", req)
-	return connect.NewResponse(&pb.AddResponse{C: req.Msg.GetA() + req.Msg.GetB()}), nil
+	res := connect.NewResponse(&pb.AddResponse{C: req.Msg.GetA() + req.Msg.GetB()})
+	s.logger.Info().Interface("request", req).Interface("response", res).Send()
+	return res, nil
 }
 
 func main() {
-	s := &server{}
-	path, handler := pbConn.NewCalculatorHandler(s)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Caller().Timestamp().Logger().Level(zerolog.DebugLevel)
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Use(cors.Handler(cors.Options{AllowedHeaders: []string{"*"}}))
-	router.Handle(path+"*", handler)
+	s := &server{logger}
+
+	mux := http.NewServeMux()
+	path, connectHandler := connectPb.NewCalculatorHandler(s)
+	c := cors.New(cors.Options{AllowedHeaders: []string{"*"}})
+	handler := c.Handler(connectHandler)
+	mux.Handle(path, handler)
 
 	// use h2c so we can serve HTTP/2 without TLS
-	if err := http.ListenAndServe("0.0.0.0:3000", h2c.NewHandler(router, &http2.Server{})); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:3000", h2c.NewHandler(mux, &http2.Server{})); err != nil {
 		log.Fatalf("http.ListenAndServe: %v\n", err)
 	}
 }
