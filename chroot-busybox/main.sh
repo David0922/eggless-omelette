f1c11dba91c802498c5516faac4bd4e11d9fe525
David Chen <36295559+David0922@users.noreply.github.com>
2026-02-04 00:34:24 +0800

    -

--- chroot-busybox/main.sh
+++ chroot-busybox/main.sh
@@ -2,16 +2,25 @@
 
 set -e -x
 
-rm -rf ./root
-mkdir -p ./root/bin ./root/etc
+sudo umount ./root/dev || true
+sudo umount ./root/proc || true
+
+sudo rm -rf ./root
+mkdir -p ./root/{bin,dev,etc,proc,tmp}
+
+sudo mount --bind /dev ./root/dev
+sudo mount --types proc proc ./root/proc
 
 echo "$(id -gn):x:$(id -g):" >> ./root/etc/group
 echo "$(id -un):x:$(id -u):$(id -g)::/:/bin/sh" >> ./root/etc/passwd
 
-echo 'nameserver 8.8.8.8' >> ./root/etc/resolv.conf
 echo 'nameserver 1.1.1.1' >> ./root/etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> ./root/etc/resolv.conf
 
 podman build -f busybox.Dockerfile -t install-busybox
 podman run -v $(realpath ./root):/build install-busybox
 
-# sudo chroot --userspec=$(id -u):$(id -g) ./root /bin/sh
+mkdir -p ./root/etc/ssl/certs
+curl -fsSL https://curl.se/ca/cacert.pem -o ./root/etc/ssl/certs/ca-certificates.crt
+
+: 'sudo chroot --userspec=$(id -u):$(id -g) ./root /bin/sh -l'
